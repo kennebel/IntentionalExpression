512c42cd74e593b0a39b339420f7046afa987a76
Jeremy A. Cunningham <dev@kennebel.com>
2016-01-14 14:02:15 -0700

    More prep work

---
+++ IntentionalExpression/Support/BaseNotify.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntentionalExpression.Support
+{
+	public class BaseNotify : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName]string propChanged = null)
+		{
+			if (PropertyChanged != null && !String.IsNullOrEmpty(propChanged))
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propChanged));
+			}
+		}
+		#endregion
+	}
+}
---
+++ IntentionalExpression/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using IntentionalExpression.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntentionalExpression.ViewModels
+{
+	public class MainWindowViewModel : BaseNotify
+	{
+	}
+}
